2ea743e4cfa4e52549c75b9dd52ee782a5b098cc
Jan Bína <binajohny@gmail.com>
2023-02-26 19:07:29 +0100

    Dont display command duration in fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 set --global hydro_color_pwd $fish_color_param
 set --global hydro_color_git $fish_color_keyword
 set --global fish_prompt_pwd_dir_length 3
-
+set --global hydro_cmd_duration_threshold 1000000000
