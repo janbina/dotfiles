cc53779c0dbf66e0c897eb3cfd15830970e332eb
Jan Bína <binajohny@gmail.com>
2023-02-26 09:18:03 +0100

    Hydro fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,4 @@
-eval (ssh-agent -c) >> /dev/null
-ssh-add -q ~/.ssh/bitbucket
-ssh-add -q ~/.ssh/github
-ssh-add -q ~/.ssh/gitlab
-ssh-add -q ~/.ssh/seznam
-
-set PATH ~/scripts ~/bin ~/.local/share/gem/ruby/3.0.0/bin ~/go/bin ~/.cargo/bin $PATH
-
-# Hide the fish greeting
-set fish_greeting ""
-
-starship init fish | source
+set --global hydro_color_pwd $fish_color_param
+set --global hydro_color_git $fish_color_keyword
+set --global fish_prompt_pwd_dir_length 3
 
